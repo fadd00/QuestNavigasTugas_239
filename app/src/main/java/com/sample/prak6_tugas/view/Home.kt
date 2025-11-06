b8b695f51f7d77f603cff5cd969a45a60a7365d5
fadd00 <andhikahutama9@gmail.com>
2025-11-07 04:22:26 +0700

    feat: Add HomePage composable function to Home.kt

--- app/src/main/java/com/sample/prak6_tugas/view/Home.kt
+++ app/src/main/java/com/sample/prak6_tugas/view/Home.kt
@@ -1,2 +1,14 @@
 package com.sample.prak6_tugas.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage(
+    onNavigateToForm: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
