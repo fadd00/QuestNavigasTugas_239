4491a7654a125db7c14abf9a1c94cebdb8b9a52a
fadd00 <andhikahutama9@gmail.com>
2025-11-07 04:10:50 +0700

    fix: Import missing resource for TampilData functionality

--- app/src/main/java/com/sample/prak6_tugas/view/TampilData.kt
+++ app/src/main/java/com/sample/prak6_tugas/view/TampilData.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sample.prak6_tugas.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
