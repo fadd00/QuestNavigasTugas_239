66b0472335c89fb815ffdb1aadd5d41274f161f9
fadd00 <andhikahutama9@gmail.com>
2025-11-07 04:23:06 +0700

    feat: Implement Scaffold and TopAppBar in Home composable

--- app/src/main/java/com/sample/prak6_tugas/view/Home.kt
+++ app/src/main/java/com/sample/prak6_tugas/view/Home.kt
@@ -1,8 +1,20 @@
 package com.sample.prak6_tugas.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +22,26 @@ fun HomePage(
     onNavigateToForm: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ){
+            paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
 
+        }
+    }
 }
