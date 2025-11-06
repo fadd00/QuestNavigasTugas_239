5f832544945c6529f8b476913fa33e6dafde0ed4
fadd00 <andhikahutama9@gmail.com>
2025-11-07 04:01:18 +0700

    feat: Enhance Navigasi with navigation components and NavHost setup

--- app/src/main/java/com/sample/prak6_tugas/Navigasi.kt
+++ app/src/main/java/com/sample/prak6_tugas/Navigasi.kt
@@ -4,6 +4,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 enum class navigasi {
     Home,
