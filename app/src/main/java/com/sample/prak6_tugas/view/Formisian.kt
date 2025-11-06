3dc8ee412fa1452586dcf75fa1002e12b415dcb1
fadd00 <andhikahutama9@gmail.com>
2025-11-07 04:13:33 +0700

    feat: Add Scaffold and TopAppBar to Formisian for enhanced UI

--- app/src/main/java/com/sample/prak6_tugas/view/Formisian.kt
+++ app/src/main/java/com/sample/prak6_tugas/view/Formisian.kt
@@ -1,5 +1,9 @@
 package com.sample.prak6_tugas.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,8 +15,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,6 +49,16 @@ fun FormDataDiri(
             )
         }
     ){
+            paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 24.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
 
+        }
     }
 }
