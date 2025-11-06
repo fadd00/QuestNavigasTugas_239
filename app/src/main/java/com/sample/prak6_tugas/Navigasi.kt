5a7de7347c9e36b95a013616a5ff882a30654e59
fadd00 <andhikahutama9@gmail.com>
2025-11-07 03:57:20 +0700

    feat: Add DataApp composable
    Add a new, empty composable function named `DataApp` in `Navigasi.kt`. This function accepts an optional `Modifier` and a `NavHostController`.

--- app/src/main/java/com/sample/prak6_tugas/Navigasi.kt
+++ app/src/main/java/com/sample/prak6_tugas/Navigasi.kt
@@ -1,7 +1,18 @@
 package com.sample.prak6_tugas
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class navigasi {
     Home,
     Formulir,
     Detail
+}
+
+@Composable
+fun DataApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+
 }
